1791021329b8aadc8e4420fbc3577baa933587e2
Spiridonov Leonid <50545443+GreatoYare@users.noreply.github.com>
2021-12-23 10:41:26 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int value = 0;
         while (value != -1) {
-            System.out.println("Поиск: 1 - ограниченный в глубину, 2 - двунаправленный, 3 - выход");
+            System.out.println("Поиск: 1 - ограниченный в глубину, 2 - по критерию стоимости, 3 - выход");
             System.out.print("Ввод: ");
             value = new Scanner(System.in).nextInt();
             switch (value) {
